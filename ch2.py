49d4323556811431b0d58d34b8984e7d37178df7
AMARNATH KOLLA <113220074+Amar269@users.noreply.github.com>
2025-02-07 19:05:23 +0530

    Add files via upload

---
+++ ch2.py
@@ -0,0 +1,50 @@
+#this is ch2 it have strings and conditinal staements if and else.
+"""  code abt n , t using in code
+str1 = "this is a sting and .\t it is created in python "
+str2 = "this is a string and \n for nxt line "
+print(str1)
+print(str2) """
+
+#str1 = "amarnath"
+#print(str1[4])
+
+"""amar = "redmi is super ra  "
+print(amar [1:4])
+print(amar [ :4])
+print(amar [3 : len(amar)])"""
+
+"""str = "i am a cloud geek "
+
+print(str.endswith("ek"))
+print(str.replace("a" , "0"))
+print(str.capitalize())
+print(str.find("cloud"))
+print(str.count("a"))"""
+
+#NAME= str(input("name = "))
+#print(len(NAME))
+
+#pratice question for elese if.
+
+""" 1.) num = int(input("number :"))
+if(num%2 == 0):
+    print("even")
+else:
+    print("odd")"""
+
+"""num = int(input("number :"))
+if (num % 7 == 0):
+    print("multiple of 7")
+else:
+    print("not multiple of 7")"""
+
+#three input and find the graet number
+a = int(input("enter 1st number  : "))
+b = int(input("enter the 2nd number : "))
+c = int(input("enter the 3rd number : "))
+if (a>b == a>>c):
+    print("a is big number " , a)
+elif(b>c):
+    print("b is big number among all " , b)
+else:
+    print("c is the gratest number in given numbers " , c)
